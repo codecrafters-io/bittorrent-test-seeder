60b5a2910fa732962a134726925c0b5814c89bee
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-08-22 18:48:33 +0100

    Add aria2 support to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM alpine:latest
 
 RUN apk add --no-cache transmission-cli
+RUN apk add --no-cache aria2
 
 ADD torrent_files /etc/torrent_files
-ADD transmission-settings.json /root/.config/transmission/settings.json
-ADD seeder.sh /bin/seeder.sh
+# ADD transmission-settings.json /root/.config/transmission/settings.json
+# ADD seeder.sh /bin/seeder.sh
 
 CMD ["/bin/seeder.sh"]
--- seeder.sh
+++ seeder.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 set -e
-exec transmission-cli /etc/torrent_files/congratulations.gif.torrent -w /etc/torrent_files -p 51413
+# exec transmission-cli /etc/torrent_files/congratulations.gif.torrent -w /etc/torrent_files -p 51413
+exec aria2
+exec aria2c \
+    -V \
+    --seed-time=0 \
+    --listen-port=51413 \
+    --enable-dht=false \
+    --enable-peer-exchange=false \
+    --bt-enable-lpd=false \
+    --bt-external-ip="137.66.12.135" \
+    -d /etc/torrent_files \
+    congratulations.gif.torrent
