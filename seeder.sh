19aade34dc4bbc7b2bc971857cf5d7e201d35633
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-08-22 18:53:20 +0100

    Update seeder.sh to set seed ratio to 0.0

--- seeder.sh
+++ seeder.sh
@@ -3,7 +3,7 @@ set -e
 # exec transmission-cli /etc/torrent_files/congratulations.gif.torrent -w /etc/torrent_files -p 51413
 exec aria2c \
     -V \
-    --seed-time=0 \
+    --seed-ratio=0.0 \
     --listen-port=51413 \
     --enable-dht=false \
     --enable-peer-exchange=false \
