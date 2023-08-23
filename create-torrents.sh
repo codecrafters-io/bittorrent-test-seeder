3b9615564332e13e1728b6bde3e61491dd895220
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-08-23 15:50:28 +0100

    remove creation dates

--- create-torrents.sh
+++ create-torrents.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -e
 
-
 rm -rf torrent_files/*.torrent
-mktorrent --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/codercat.gif.torrent torrent_files/codercat.gif/codercat.gif
-mktorrent --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/congratulations.gif.torrent torrent_files/congratulations.gif/congratulations.gif
-mktorrent --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/itsworking.gif.torrent torrent_files/itsworking.gif/itsworking.gif
+mktorrent -d --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/codercat.gif.torrent torrent_files/codercat.gif/codercat.gif
+mktorrent -d --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/congratulations.gif.torrent torrent_files/congratulations.gif/congratulations.gif
+mktorrent -d --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/itsworking.gif.torrent torrent_files/itsworking.gif/itsworking.gif
