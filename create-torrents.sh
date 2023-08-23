51d3069b2e8f618f7ae277105d6f08b0b3c1a87e
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-08-23 15:09:40 +0100

    Update create-torrents.sh script

--- create-torrents.sh
+++ create-torrents.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-set -e 
+set -e
 
+
+rm -rf torrent_files/*.torrent
 mktorrent -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/codercat.gif.torrent torrent_files/codercat.gif/codercat.gif
+mktorrent -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/congratulations.gif.torrent torrent_files/congratulations.gif/congratulations.gif
+mktorrent -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/itsworking.gif.torrent torrent_files/itsworking.gif/itsworking.gif
