30aaf987008fdc6842cbd781aeefb6c76dcb8dac
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-08-23 15:12:05 +0100

    update torrent files

--- create-torrents.sh
+++ create-torrents.sh
@@ -3,6 +3,6 @@ set -e
 
 
 rm -rf torrent_files/*.torrent
-mktorrent -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/codercat.gif.torrent torrent_files/codercat.gif/codercat.gif
-mktorrent -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/congratulations.gif.torrent torrent_files/congratulations.gif/congratulations.gif
-mktorrent -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/itsworking.gif.torrent torrent_files/itsworking.gif/itsworking.gif
+mktorrent --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/codercat.gif.torrent torrent_files/codercat.gif/codercat.gif
+mktorrent --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/congratulations.gif.torrent torrent_files/congratulations.gif/congratulations.gif
+mktorrent --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/itsworking.gif.torrent torrent_files/itsworking.gif/itsworking.gif
