9c8f91d6bb39f5f9de581a60ef91c2b84f5c3bed
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-08-24 13:02:01 +0100

    update

--- create-torrents.sh
+++ create-torrents.sh
@@ -6,4 +6,4 @@ rm -rf torrent_files/*.torrent
 mktorrent -d --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/codercat.gif.torrent torrent_files/codercat.gif/codercat.gif
 mktorrent -d --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/congratulations.gif.torrent torrent_files/congratulations.gif/congratulations.gif
 mktorrent -d --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/itsworking.gif.torrent torrent_files/itsworking.gif/itsworking.gif
-mktorrent -d --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/sample.txt.torrent torrent_files/sample.txt/sample.txt
+mktorrent -d --piece-length=15 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/sample.txt.torrent torrent_files/sample.txt/sample.txt
