157d25253fb18fb5690f1a8876a6a3bfef67c5d0
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-08-24 12:56:53 +0100

    Create torrent files for codercat.gif, congratulations.gif, and itsworking.gif

--- create-torrents.sh
+++ create-torrents.sh
@@ -3,7 +3,6 @@ set -e
 
 rm -rf torrent_files/*.torrent
 
-
 mktorrent -d --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/codercat.gif.torrent torrent_files/codercat.gif/codercat.gif
 mktorrent -d --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/congratulations.gif.torrent torrent_files/congratulations.gif/congratulations.gif
 mktorrent -d --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/itsworking.gif.torrent torrent_files/itsworking.gif/itsworking.gif
