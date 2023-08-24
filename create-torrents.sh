5515cad58179db5f0f21ee69e1b8d365a93723ee
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-08-24 12:56:19 +0100

    Add new torrent file and sample text file

--- create-torrents.sh
+++ create-torrents.sh
@@ -2,6 +2,9 @@
 set -e
 
 rm -rf torrent_files/*.torrent
+
+
 mktorrent -d --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/codercat.gif.torrent torrent_files/codercat.gif/codercat.gif
 mktorrent -d --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/congratulations.gif.torrent torrent_files/congratulations.gif/congratulations.gif
 mktorrent -d --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/itsworking.gif.torrent torrent_files/itsworking.gif/itsworking.gif
+mktorrent -d --piece-length=18 -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/sample.txt.torrent torrent_files/sample.txt/sample.txt
