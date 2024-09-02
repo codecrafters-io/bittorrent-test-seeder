44307355bf13550c903df36562cc61fbef1136a0
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-09-02 17:57:01 +0100

    use separate folders

--- create-torrents.sh
+++ create-torrents.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 set -e
 
-rm -rf torrent_files/*.torrent
+rm -rf torrent_files/**/*.torrent
 rm -rf torrent_files/magnet_links.txt
 
-for folder in torrent_files/*; do
-    if [ ! -d "$folder" ]; then
-        continue
-    fi
+for seeder_type in base magnet; do
+    for folder in torrent_files/$seeder_type/*; do
+        if [ ! -d "$folder" ]; then
+            continue
+        fi
 
-    file=$(basename "$folder")
+        file=$(basename "$folder")
 
-    if [ "$file" = "sample.txt" ]; then
-        piece_length=15
-    else
-        piece_length=18
-    fi
+        if [ "$file" = "sample.txt" ]; then
+            piece_length=15
+        else
+            piece_length=18
+        fi
 
-    mktorrent -d --piece-length=$piece_length -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/$file.torrent torrent_files/$file/$file
-    echo "$file: $(transmission-show -m torrent_files/$file.torrent)" >> torrent_files/magnet_links.txt
+        mktorrent -d --piece-length=$piece_length -a http://bittorrent-test-tracker.codecrafters.io/announce -o torrent_files/$seeder_type/$file.torrent torrent_files/$seeder_type/$file/$file
+        echo "$seeder_type/$file: $(transmission-show -m torrent_files/$seeder_type/$file.torrent)" >>torrent_files/magnet_links.txt
+    done
 done
